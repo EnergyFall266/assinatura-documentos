dcd639e7f6c284b086cd10500aa97019cea20008
Leonardo Vanzin <leonardo.vanzin@hotmail.com>
2024-06-27 17:30:02 -0300

    criado parte do service para criar o envelope e para mandar para assinar

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -31,6 +31,112 @@ export class AppService {
       });
   }
 
+async createEnvelope(){
+  const axios = require('axios');
+let data = JSON.stringify({
+  "signers": [
+    {
+      "name": "Leonardo Vanzin",
+      "email": "leonardo.vanzin@prismainformatica.com.br",
+      "username": "leonardo@prisma-demo.com.br.seniorx",
+      "orderSign": 1,
+      "signerType": "PIONEER",
+      "digitalCertificate": "OPTIONAL",
+      "communicationChannel": {
+        "email": true,
+        "platform": false
+      }
+    },
+    {
+      "name": "gustavo bertoglio",
+      "email": "gustavo.bertoglio@prismainformatica.com.br",
+      "username": "gustavo.bertoglio@prisma-demo.com.br.seniorx",
+      "photo": "https://br-com-senior-blob-service-storage.s3.sa-east-1.amazonaws.com/prisma-democombrseniorx/temp/platform/user/98e7134c-11d9-331b-8311-4667abc08ab9/336f0cdb-c1cc-4bea-98f2-d13182ef8afe-user-photo.png",
+      "orderSign": 1,
+      "signerType": "PIONEER",
+      "digitalCertificate": "MANDATORY",
+      "communicationChannel": {
+        "email": true,
+        "platform": false
+      }
+    }
+  ],
+  "conectors": [],
+  "envelopeDocuments": [
+    {
+      "documentVersion": "f2a7c44d-617d-4aa5-8421-75177e41d8c3",
+      "envelopePosition": 1
+    },
+    {
+      "documentVersion": "c4e900ce-2f28-44a0-a353-1dbe73d0a523",
+      "envelopePosition": 2
+    }
+  ],
+  "envelopeDraftId": "",
+  "name": "pioneira-opicional-sem(vizualização,notificar autor e localização)",
+  "instructionsToSigner": "",
+  "documentsVersions": [
+    "f2a7c44d-617d-4aa5-8421-75177e41d8c3",
+    "c4e900ce-2f28-44a0-a353-1dbe73d0a523"
+  ],
+  "askGeolocation": "DONT_ASK_LOCATION",
+  "daysToNotify": 0,
+  "daysToExpire": 120,
+  "permissions": [],
+  "mandatoryView": false,
+  "notificateAuthor": false,
+  "searchJobPosition": false
+});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://platform.senior.com.br/t/senior.com.br/bridge/1.0/rest/platform/ecm_ged/actions/saveEnvelope',
+  headers: { 
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+try {
+  const response = await axios(config);
+  console.log(response.data);
+
+  return response.data;
+} catch (error) {
+  console.log(error);
+}
+
+}
+
+async sendToSign(){
+  const axios = require('axios');
+let data = JSON.stringify({
+  "envelopeDraftId": "6f9c13f1-aa0d-4e5a-8caf-4f2dbb66b051",
+  "notifyUser": true,
+  "sendEmail": true
+});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://platform.senior.com.br/t/senior.com.br/bridge/1.0/rest/platform/ecm_ged/actions/sendDraftToSign',
+  headers: { 
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+try {
+  const response = await axios(config);
+  console.log(response.data);
+
+  return response.data;
+} catch (error) {
+  console.log(error);
+}
+}
+
   async usuariosInternos(
     token: any,
     paginaAtual: number,
